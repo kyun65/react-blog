7d2ba2ee938c917e4ef42436f66a2ce67a2a50ea
kyun65 <kyun.anothercastle@gmail.com>
2024-10-06 14:52:58 +0900

    Routineの練習

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -3,15 +3,52 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Posts } from "./data/Posts";
+import { Routes, Route, Link, NavLink } from 'react-router-dom';
+import Home from './data/Home';
+import About from './data/About';
+import Contact from './data/Contact';
+import NoMatch from './data/nomatch';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+//   const [count, setCount] = useState(0)
 
-  return (
-    <>
-<Posts />
-    </>
-  )
+//   return (
+//     <>
+// <Posts />
+//     </>
+//   )
+return (
+
+  <div className="App">
+      <h1>Hello React Router v6</h1>
+
+
+
+
+      <ul>
+        <li>
+        <NavLink style={({ isActive }) => (isActive ? { color: 'blue' } : undefined)} to="/">Home</NavLink>
+        </li>
+        <li>
+        <NavLink className={({ isActive }) => (isActive ? 'active' : undefined)} to="/about">About</NavLink>
+        </li>
+        <li>
+        <NavLink className={({ isActive }) => (isActive ? 'active' : undefined)} to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+
+
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/contact" element={<Contact message="Hello Contact" />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+    </div>
+  );
+
 }
 
 export default App
--- blog/src/main.jsx
+++ blog/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
