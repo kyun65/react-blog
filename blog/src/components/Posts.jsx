31f0f4be57fe7edc3723c4fb6522f73dc2b150df
kyun65 <kyun.anothercastle@gmail.com>
2024-10-10 11:01:10 +0900

    各記事へのリンク設定

--- blog/src/components/Posts.jsx
+++ blog/src/components/Posts.jsx
@@ -15,7 +15,7 @@ return (
         return (
           <div className={styles.post_list} key={post.id}>
             <div className={styles.post_item}>
-              <Link to={post.id}>
+              <Link to={`/${post.id}`}>
               <div className={styles.post_list_info}>
                 <div className={styles.post_list_date} key={post.createdAt}>{post.createdAt.replace(/-/g, '/').slice( 0, 10 )}</div>
                 {post.categories.map((categorie)=>{
