dcbf10c9073a84359007ba02a77fdc5fa9ecfb51
kyun65 <kyun.anothercastle@gmail.com>
2024-10-11 14:35:17 +0900

    表示されない

--- blog/src/components/Posts.jsx
+++ blog/src/components/Posts.jsx
@@ -8,10 +8,28 @@ import { Link } from "react-router-dom";
 export const Posts = () => {
 
 
+
+  const [posts, setPosts] = useState([])
+
+  // APIでpostsを取得する処理をuseEffectで実行します。
+  useEffect(() => {
+    const fetcher = async () => {
+      const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts")
+      const data = await res.json()
+      setPosts(data.posts)
+    }
+
+    fetcher()
+  }, [])
+
+
+
+
+
 return (
 
     <div>
-      {postItems.map((post)=> {
+      {posts.map((post)=> {
         return (
           <div className={styles.post_list} key={post.id}>
             <div className={styles.post_item}>
