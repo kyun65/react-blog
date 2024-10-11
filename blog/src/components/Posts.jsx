403eaf6c94f75ec3af08d6c46193b137d0d2d903
kyun65 <kyun.anothercastle@gmail.com>
2024-10-11 16:44:07 +0900

    ブランチ整理

--- blog/src/components/Posts.jsx
+++ blog/src/components/Posts.jsx
@@ -1,16 +1,31 @@
+import { useState, useEffect } from "react"
 import styles from '../index.module.css'
-import { postItems } from "../data/Post-data";
 import { Link } from "react-router-dom";
 
-
-// /src/data/posts.js
 export const Posts = () => {
+
+
+
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    const fetcher = async () => {
+      const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts")
+      const data = await res.json()
+      setPosts(data.posts)
+      console.log(data)
+    }
+
+    fetcher()
+  }, [])
+
+
+
 
 
 return (
 
     <div>
-      {postItems.map((post)=> {
+      {posts.map((post)=> {
         return (
           <div className={styles.post_list} key={post.id}>
             <div className={styles.post_item}>
@@ -19,7 +34,7 @@ return (
                 <div className={styles.post_list_date} key={post.createdAt}>{post.createdAt.replace(/-/g, '/').slice( 0, 10 )}</div>
                 {post.categories.map((categorie)=>{
                   return (
-                    <div className={styles.post_list_category} key={categorie}>
+                    <div className={styles.post_list_category} key={post.category}>
                       {categorie}
                     </div>
                   );
