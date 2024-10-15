23433393d11f35cc84c185a5606f7d88e2287e96
kyun65 <kyun.anothercastle@gmail.com>
2024-10-15 15:56:40 +0900

    記事が無い状態で表示

--- blog/src/components/Post.jsx
+++ blog/src/components/Post.jsx
@@ -1,38 +1,56 @@
+import { useState,useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { postItems } from "../data/Post-data";
+
 import styles from '../index.module.css'
 
 export const Post = () => {
 
-const { id } = useParams(); //分割代入
+const { id } = useParams(null); //分割代入
 console.log(id)
 
-const showPost = postItems.find((post)=>{
-  const numberId = Number(id);
-  return (numberId === post.id);
+const [isLoading, setIsLoading] = useState();
+const [posts, setPosts] = useState() //useState:コンポーネントにてstateを保持・更新させる
 
-});
+// APIでpostsを取得する処理をuseEffectで実行します。
+useEffect(() => {
+  const fetcher = async () => {
+    try {
+      const res = await fetch(`https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${id}`) //外部のjsonの読み込み
+      const data = await res.json() //jsonをjsのオブジェクト形式に変換
+      setPosts(data.post) //dataの中からpostsの情報だけを抜き取る
+      console.log('情報',data) //一旦dataの中身を確認する
+      console.log('res情報',res) //一旦dataの中身を確認する
+    } finally {
+    setIsLoading(false);
+  }
+}
+  fetcher()
+}, []);
 
-if (!showPost) return <div>記事が見つかりませんでした</div>
+
+if(isLoading) {
+  return <p>Loading...</p>;
+} else if (!posts) return <div>記事が見つかりませんでした</div>
+
 
 return (
-  <>
+
   <div className={styles.detail_container}>
-    <div className={styles.detail_title}>{showPost.title}</div>
-    <img src={showPost.thumbnailUrl} alt="" />
+    <div className={styles.detail_title}>{posts.title}</div>
+    <img src={posts.thumbnailUrl} alt="" />
     <div className={styles.detail_info}>
-    <span className={styles.detail_date}>{showPost.createdAt.replace(/-/g, '/').slice( 0, 10 )}</span>
+    <span className={styles.detail_date}>{posts.createdAt.replace(/-/g, '/').slice( 0, 10 )}</span>
     <div className={styles.detail_cat_wrap}>
-    {showPost.categories.map((categorie)=>{
+    {posts.categories.map((categorie)=>{
       return(
-        <div className={styles.detail_cat}>{categorie}</div>
+        <div className={styles.detail_cat} key={categorie}>{categorie}</div>
       )
     })}
     </div>
     </div>
-    <p className={styles.detail_content} dangerouslySetInnerHTML={{ __html: showPost.content }} />
+    <p className={styles.detail_content} dangerouslySetInnerHTML={{ __html: posts.content }} />
     </div>
-  </>
+
 
 )
 
--- blog/src/components/Posts.jsx
+++ blog/src/components/Posts.jsx
@@ -1,17 +1,41 @@
-import { useState } from "react"
-import styles from '../index.module.css'
-import { postItems } from "../data/Post-data";
-import { Link } from "react-router-dom";
+import { useState,useEffect } from "react" //コンポーネントの呼び出し
+import styles from '../index.module.css' //cssの読み込みの呼び出し
+import { Link } from "react-router-dom"; //react-router-domコンポーネントのリンク機能の呼び出し
 
 
-// /src/data/posts.js
+
 export const Posts = () => {
+
+  const [isLoading, setIsLoading] = useState();
+  const [posts, setPosts] = useState([]) //useState:コンポーネントにてstateを保持・更新させる
+
+  // APIでpostsを取得する処理をuseEffectで実行します。
+  useEffect(() => {
+    const fetcher = async () => {
+      try {
+      const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts") //外部のjsonの読み込み
+      const data = await res.json() //jsonをjsのオブジェクト形式に変換
+      setPosts(data.posts) //dataの中からpostsの情報だけを抜き取る
+      console.log('情報',data) //一旦dataの中身を確認する
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+    fetcher()
+  }, [])
+
+
+  if(isLoading) {
+    return <p>Loading...</p>;
+  } else if (!posts) return <div>記事が見つかりませんでした</div>
+
 
 
 return (
 
     <div>
-      {postItems.map((post)=> {
+      {posts.map((post)=> {
         return (
           <div className={styles.post_list} key={post.id}>
             <div className={styles.post_item}>
@@ -20,7 +44,7 @@ return (
                 <div className={styles.post_list_date} key={post.createdAt}>{post.createdAt.replace(/-/g, '/').slice( 0, 10 )}</div>
                 {post.categories.map((categorie)=>{
                   return (
-                    <div className={styles.post_list_category}>
+                    <div className={styles.post_list_category} key={categorie}>
                       {categorie}
                     </div>
                   );
