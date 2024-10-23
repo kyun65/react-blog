9fbe7b92ab9f6bbbed47a652cc9f0893a6846fe5
kyun65 <kyun.anothercastle@gmail.com>
2024-10-23 15:34:28 +0900

    作業途中

--- blog/src/components/Posts.jsx
+++ blog/src/components/Posts.jsx
@@ -41,24 +41,31 @@ return (
 
 
     <div>
-     
-   
+        {postItem.map((post)=> {
+          return(
           <div className={styles.post_list} key={post.id}>
-            <div className={styles.post_item}>
-              <div className={styles.post_list_info}>
-                <div className={styles.post_list_date} key={post.createdAt}>ああ</div>
-              
-              </div>
-              <div className={styles.post_list_title}>
-                <div></div>
-              </div>
-              <div className={styles.post_list_content}>
-                <div dangerouslySetInnerHTML={{ __html: post.content }} />
-              </div>
+          <div className={styles.post_item}>
+            <div className={styles.post_list_info}>
+              <div className={styles.post_list_date} key={post.createdAt}>{post.createdAt.replace(/-/g, '/').slice( 0, 10 )}</div>
+            {post.categories.map((categorie)=>{
+              return(
+                <div className={styles.post_list_category} key={categorie}>
+                   {categorie}
+           </div>
+              )
+            })}
+
+            </div>
+            <div className={styles.post_list_title}>
+              <div>{post.title}</div>
+            </div>
+            <div className={styles.post_list_content}>
+              <div dangerouslySetInnerHTML={{ __html: post.content }} />
             </div>
           </div>
-        )
-  
+        </div>
+          )
+     })}
     </div>
 
 
