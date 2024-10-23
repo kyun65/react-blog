07186d02613f80f67ec0e3a93b84f2fb2eaeb530
kyun65 <kyun.anothercastle@gmail.com>
2024-10-23 15:14:00 +0900

    練習用のブランチを用意

---
+++ blog/src/components/Posts.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react"
+import styles from '../index.module.css'
+import { postItem } from '../data/DataTwo.js'
+
+
+// /src/data/posts.js
+export const Posts = () => {
+
+
+
+
+
+return (
+
+    // <div>
+    //   {postItem.map((post)=> {
+    //     return (
+    //       <div className={styles.post_list} key={post.id}>
+    //         <div className={styles.post_item}>
+    //           <div className={styles.post_list_info}>
+    //             <div className={styles.post_list_date} key={post.createdAt}>{post.createdAt.replace(/-/g, '/').slice( 0, 10 )}</div>
+    //             {post.categories.map((categorie)=>{
+    //               return (
+    //                 <div className={styles.post_list_category} key={categorie}>
+    //                   {categorie}
+    //                 </div>
+    //               );
+    //             })}
+    //           </div>
+    //           <div className={styles.post_list_title}>
+    //             <div>{post.title}</div>
+    //           </div>
+    //           <div className={styles.post_list_content}>
+    //             <div dangerouslySetInnerHTML={{ __html: post.content }} />
+    //           </div>
+    //         </div>
+    //       </div>
+    //     )
+    //   })}
+    // </div>
+
+
+    <div>
+     
+   
+          <div className={styles.post_list} key={post.id}>
+            <div className={styles.post_item}>
+              <div className={styles.post_list_info}>
+                <div className={styles.post_list_date} key={post.createdAt}>ああ</div>
+              
+              </div>
+              <div className={styles.post_list_title}>
+                <div></div>
+              </div>
+              <div className={styles.post_list_content}>
+                <div dangerouslySetInnerHTML={{ __html: post.content }} />
+              </div>
+            </div>
+          </div>
+        )
+  
+    </div>
+
+
+);
+
+}
